151c2dfbe4dbaedbf16d68b45f72627a7dc792e4
Tsuyoshi Yamaura <tyamaura@riken.jp>
2025-11-11 21:34:15 +0900

    update setup_spack.sh

--- setup_spack.sh
+++ setup_spack.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-. /vol0004/apps/oss/spack/share/spack/setup-env.sh
+. /home/apps/oss/spack/share/spack/setup-env.sh
 
-NC_HASH=`spack find -lx netcdf-c%fj@4.10.0 | grep netcdf-c | awk '{print $1}'`
-NF_HASH=`spack find -lx netcdf-fortran%fj@4.10.0 | grep netcdf-fortran | awk '{print $1}'`
+NC_HASH=`spack find -lx netcdf-c%fj@4.12.0 | grep netcdf-c | awk '{print $1}'`
+NF_HASH=`spack find -lx netcdf-fortran%fj@4.12.0 | grep netcdf-fortran | awk '{print $1}'`
 HDF_HASH=`spack find -l --deps /${NC_HASH} | grep hdf5 | awk '{print $1}'`
 SCALE_HDF=`spack location --install-dir /${HDF_HASH}`
 SCALE_NETCDF_C=`spack location --install-dir /${NC_HASH}`
 SCALE_NETCDF_F=`spack location --install-dir /${NF_HASH}`
-SCALE_PNETCDF=`spack location --install-dir parallel-netcdf%fj@4.10.0`
+SCALE_PNETCDF=`spack location --install-dir parallel-netcdf%fj@4.12.0`
 
 export SCALE_DB="`pwd`/scale_database"
 export SCALE_NETCDF_INCLUDE="-I${SCALE_NETCDF_C}/include -I${SCALE_NETCDF_F}/include -I${SCALE_PNETCDF}/include"
